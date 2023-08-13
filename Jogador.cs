86088b105b08b3f7341cf0a4867dfbc8afde52c9
Narciso <nrcsfo@gmail.com>
2023-08-13 18:09:21 -0300

    Add project files.

---
+++ Jogador.cs
@@ -0,0 +1,41 @@
+﻿public class Jogador
+{
+    public int energia;
+    public bool vivo;
+    public string? nome;
+
+    // Construtor com expression body
+    public Jogador(bool vivo) => this.vivo = vivo;
+
+    // Construtor vazio
+    public Jogador() { }
+
+    // Construtor padrão
+    public Jogador(int energia, bool vivo, string nome) 
+    {
+        this.vivo = vivo;
+        this.energia = energia;
+        this.nome = nome;
+        
+    }
+
+    // primeiro método
+    public void chute()
+    {
+      Console.WriteLine($"{nome} está chutando");
+    }
+
+    // segundo método
+
+    public void correr()
+    {
+      Console.WriteLine($"{nome} está correndo");
+    }
+
+    // terceiro método com argumentos
+
+    public void passe(string nome2)
+    {
+      Console.WriteLine($"{nome} está passando para o jogador {nome2}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("## Classes e Métodos ##");
+
+Jogador jogador = new Jogador();
+
+jogador.energia = 100;
+jogador.vivo = true;
+jogador.nome = "Bruno";
+
+Console.WriteLine($"Nome do jogador: {jogador.nome}");
+Console.WriteLine($"Energia do jogador: {jogador.energia}");
+Console.WriteLine($"Status do jogador: {jogador.vivo}");
+
+// Alocação de memória
+
+// Instanciação de um objeto com o mesmo local na memória de outro objeto.
+Jogador jogador2 = jogador;
+
+Console.WriteLine("Exibindo valores do segundo objeto");
+Console.WriteLine($"Nome do jogador: {jogador2.nome}");
+Console.WriteLine($"Energia do jogador: {jogador2.energia}");
+Console.WriteLine($"Status do jogador: {jogador2.vivo}");
+
+// Instaciando um novo objeto de maneiras diferentes
+
+var jogador3 = new Jogador();
+
+jogador3.energia = 50;
+jogador3.vivo = true;
+jogador3.nome = "Lucas";
+
+Jogador jogador4 = new()
+{
+  energia = 50,
+  vivo = false,
+  nome = "João"
+};
+
+Console.WriteLine("Exibindo valores do terceiro objeto");
+Console.WriteLine($"Nome do jogador: {jogador3.nome}");
+Console.WriteLine($"Energia do jogador: {jogador3.energia}");
+Console.WriteLine($"Status do jogador: {jogador3.vivo}");
+
+
+// Métodos
+
+jogador.chute();
+
+jogador2.correr();
+
+jogador3.passe(jogador4.nome);
+
+Console.ReadLine();
